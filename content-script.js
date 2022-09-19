e8cc2e83d453467e08f4949a9d510411f3d23661
Doyeon Guk <54295374+Jenguk42@users.noreply.github.com>
2022-09-19 21:03:06 +1000

    added V2

--- content-script.js
+++ content-script.js
@@ -1,84 +1,132 @@
 {
-  let excludedNames;
-  // // Names to remove
-  // chrome.storage.local.get(["excludedNames"], function (result) {
-  //   excludedNames = result.excludedNames;
-  //   console.log("Value currently is " + result.excludedNames);
-  // });
-  // console.log(excludedNames);
+  //// Functions
+  // Shuffle the given array
+  function shuffle(array) {
+    var shuffled = array
+      .map((a) => [Math.random(), a])
+      .sort((a, b) => a[0] - b[0])
+      .map((a) => a[1]);
+    return shuffled;
+  }
+
+  // Stringify the given array
+  function stringify(array) {
+    var stringArray = "";
+    array.forEach((element) => (stringArray += `${element}, `));
+    return stringArray.substring(0, stringArray.length - 2);
+  }
 
   // Get member list
   const ulObject = document.querySelector(
     "#content > div > div.memberWrap > div._memberListWrap > div > ul"
   );
   let memberList = [];
-  let coupleList = [];
-  let soloList = [];
+  let autoCoupleList = [];
+  let noStatusList = [];
 
   // Loop through members and get the user name
   const numMems = ulObject.childElementCount;
 
-  let numCouples = 0;
+  let autoNumCouples = 0;
   let i = 0;
   while (i < numMems) {
     const username = ulObject.children[i].dataset.user_name;
 
-    //Discard username '.'
+    // Discard username '.'
     if (username !== ".") {
-
-      // Get profile message to check if it contains emoji
-      // WARNING: a member is only valid if they have a profile msg
-      // A member is a couple if they contain an emoji in their profile msg
-
       const profilemsg = ulObject.children[i].children[1].children[1];
 
-      if (typeof(profilemsg) != 'undefined' && profilemsg != null) {
-        memberList.push(username);
-        
+      // All members with or without status are added for now
+      memberList.push(username);
+
+      if (typeof (profilemsg) != 'undefined' && profilemsg != null) {
+        // A member is a couple if they contain an emoji in their profile msg
         if (/\p{Extended_Pictographic}/u.test(`${profilemsg.innerText}`)) {
-          coupleList.push(ulObject.children[i].dataset.user_name);
-          numCouples++;
-        } else {
-          soloList.push(ulObject.children[i].dataset.user_name);
+          autoCoupleList.push(ulObject.children[i].dataset.user_name);
+          autoNumCouples++;
         }
+      } else {
+        noStatusList.push(ulObject.children[i].dataset.user_name);
       }
     }
-
     i++;
   }
 
-  // Shuffle the array
-  function shuffle(array) {
-    var shuffled = array
-      .map((a) => [Math.random(), a])
-      .sort((a, b) => a[0] - b[0])
-      .map((a) => a[1]);
+  // Read data from local storage
+  const readLocalStorage = async (key) => {
+    return new Promise((resolve, reject) => {
+      chrome.storage.local.get([key], function (data) {
+        if (data[key] === undefined) {
+          reject();
+        } else {
+          resolve(data[key]);
+        }
+      });
+    });
+  };
 
-    return shuffled;
+  async function getData() {
+    emptyAdded = await readLocalStorage("emptyAdded");
+    version = await readLocalStorage("version");
+    manualCoupleList = await readLocalStorage("manualCoupleList");
+    return [emptyAdded, version, manualCoupleList];
   }
+  let data = getData();
 
-  // Stringify the array
-  function stringify(array) {
-    var stringArray = "";
-    array.forEach((element) => (stringArray += `${element}, `));
+  data.then((result) => {
+    let emptyAdded = result[0];
+    let version = result[1];
+    let manualCoupleList = result[2];
 
-    return stringArray.substring(0, stringArray.length - 2);
-  }
+    // Check emptyAdded
+    if (!emptyAdded) {
+      // Members w/o profile msg are not valid
+      memberList = memberList.filter((el) => !noStatusList.includes(el));
+      autoCoupleList = autoCoupleList.filter((el) => !noStatusList.includes(el));
+      console.log('상메 있어야 추가됨')
+    } else {
+      // Members w/o profile msg are still valid
+      console.log('상메 없어도 추가됨')
+    }
 
-  allMembersShuffled = shuffle(memberList);
-  soloShuffled = shuffle(soloList);
+    // Set solo list according to version
+    let soloList = [];
+    let manNumCouples = 0;
+    let manualCoupleListArr = [];
+    if (version === 1) {
+      // solo list = member list - couple list
+      soloList = memberList.filter((el) => !autoCoupleList.includes(el));
+    } else if (version === 2) {
+      // retrieve solo list from input
+      console.log(manualCoupleList);
+      manualCoupleListArr = manualCoupleList.split(/[\s]*[,][\s]*/);
+      soloList = memberList.filter((el) => !manualCoupleListArr.includes(el));
+      manNumCouples = memberList.length - soloList.length;
+    }
 
-  console.log(`총 멤버 수: ${memberList.length}`);
-  console.log(`총 커플 수: ${numCouples}`);
-  console.log(`커플 목록: ${stringify(coupleList)}`);
-  console.log(`커플 제외 멤버 랜덤 태그: ${stringify(soloShuffled)}`);
-  console.log(`전체 멤버 랜덤 태그: ${stringify(allMembersShuffled)}`);
+    // console.log('member list', memberList);
+    // console.log('couple list', autoCoupleList);
+    // console.log('no status list', noStatusList);
+    // console.log('solo list', soloList);
 
-  chrome.storage.local.set({
-    memberCount: memberList.length,
-    coupleCount: numCouples,
-    coupleList: stringify(coupleList),
-    soloRandom: stringify(soloShuffled),
-    allRandom: stringify(allMembersShuffled),
-  });
+    allMembersShuffled = shuffle(memberList);
+    soloShuffled = shuffle(soloList);
+
+    console.log(`총 멤버 수: ${memberList.length}`);
+    console.log(`자동 감지 커플 수: ${autoNumCouples}`);
+    console.log(`자동 감지 커플 목록: ${stringify(autoCoupleList)}`);
+    console.log(`입력된 커플 수: ${manNumCouples}`)
+    console.log(`입력된 커플 목록: ${stringify(manualCoupleListArr)}`);
+    console.log(`커플 제외 멤버 랜덤 태그: ${stringify(soloShuffled)}`);
+    console.log(`전체 멤버 랜덤 태그: ${stringify(allMembersShuffled)}`);
+
+    chrome.storage.local.set({
+      memberCount: memberList.length,
+      autoCoupleCount: autoNumCouples,
+      manCoupleCount: manNumCouples,
+      autoCoupleList: stringify(autoCoupleList),
+      soloRandom: stringify(soloShuffled),
+      allRandom: stringify(allMembersShuffled),
+    });
+  })
 }
--- popup.js
+++ popup.js
@@ -16,68 +16,168 @@ $(document).ready(function () {
     }
   });
 
-  // 페이지에서 나가면 저장된 데이터 삭제
-  chrome.tabs.onUpdated.addListener(() => {
+  // 상메 없는 멤버 확인
+  function checkEmptyAdded() {
+    if ($("#statusBlank").prop("checked")) {
+      chrome.storage.local.set({"emptyAdded": true});
+      console.log("상메 없는 멤버 추가됨");
+    } else {
+      chrome.storage.local.set({"emptyAdded": false});
+    }
+  }
+
+  // Local storage 초기화
+  function clearLocalStorage() {
     chrome.storage.local.set({
+      version: 0,
       memberCount: 0,
-      coupleCount: 0,
-      coupleList: "",
+      autoCoupleCount: 0,
+      manCoupleCount: 0,
+      autoCoupleList: "",
+      manualCoupleList: "",
       soloRandom: "",
       allRandom: "",
+      emptyAdded: false
     });
-  });
-    
-  function runScript() {
-  // 제외할 이름 저장
-  // chrome.storage.local.set(
-  //   {
-  //     excludedNames: $("#excludedNames").val(),
-  //   },
-  //   function () {
-  //     console.log(`${excludedNames.value} excluded`);
-  //   }
-  // );
+  }
 
-  // Execute content script
-  chrome.scripting.executeScript(
-    {
-      target: { tabId },
-      files: ["content-script.js"],
-    },
-    () => {
-      // Get the stored values
-      chrome.storage.local.get(
-        [
-          "memberCount",
-          "coupleCount",
-          "coupleList",
-          "soloRandom",
-          "allRandom",
-        ],
-        function (data) {
-          $("#output0").text(
-            `총 멤버 수: ${data.memberCount} / 총 커플 수: ${data.coupleCount}`
-          );
-          $("#output0").val(data.coupleList);
-          $("#output1").val(data.soloRandom);
-          $("#output2").val(data.allRandom);
-        }
-      );
-      console.log("output1 length is: " + $("#output1").val().length);
-    }
-  )}
-
-  $("#createShuffle").click(
+  // Copy text to clipboard
+  $('#copy1').click(
     function() {
-      runScript();
-      $("#return").css('display', 'flex');
+      $('#output1').select();
+      var copy = document.execCommand('copy');
+      alert("복사 완료!");
+    }
+  )
+  $('#copy2').click(
+    function() {
+      $('#output2').select();
+      var copy = document.execCommand('copy');
+      alert("복사 완료!");
+    }
+  )
+  $('#copy3').click(
+    function() {
+      $('#V2output1').select();
+      var copy = document.execCommand('copy');
+      alert("복사 완료!");
+    }
+  )
+  $('#copy4').click(
+    function() {
+      $('#V2output2').select();
+      var copy = document.execCommand('copy');
+      alert("복사 완료!");
+    }
+  )
+
+  // 페이지에서 나가면 저장된 데이터 삭제
+  chrome.tabs.onUpdated.addListener(() => {
+    clearLocalStorage();
+  });
+
+  // 1.0.0: 이모지 커플로 인식
+  $("#createShuffleV1").click(
+    function() {
+      runScriptV1();
       $("#firstClick").hide();
+      $("#returnV1").css('display', 'flex');
     });
 
   $("#newShuffle").click(
     function() {
-      runScript();
+      runScriptV1();
     });
 
-  
+  // 1.0.0 script
+  function runScriptV1() {
+    // Add version & emptyAdded
+    chrome.storage.local.set({"version": 1});
+    checkEmptyAdded();
+
+    // Execute content script
+    chrome.scripting.executeScript(
+      {
+        target: { tabId },
+        files: ["content-script.js"],
+      },
+      () => {
+        // Get the stored values
+        chrome.storage.local.get(
+          [
+            "memberCount",
+            "autoCoupleCount",
+            "autoCoupleList",
+            "soloRandom",
+            "allRandom",
+          ],
+          function (data) {
+            // Add counts
+            if($('#counts')) {
+              $("#counts").remove();
+            }
+            $('#input-container1').after('<div class="input-container" id="counts"></div>');
+            $('#counts').append("<b> 총 멤버 수: " + data.memberCount + "<br> 총 커플 수: " + data.autoCoupleCount + "</b>");
+
+            // Output values
+            $("#output0").val(data.autoCoupleList);
+            $("#output1").val(data.soloRandom);
+            $("#output2").val(data.allRandom);
+          }
+        );
+      })
+    }
+    
+  // 2.0.0: 커플 수동 입력
+  $("#createShuffleV2").click(
+    function() {
+      runScriptV2();
+      $("#firstClick").hide();
+      $("#returnV2").css('display', 'flex');
+    });
+
+  $("#runShuffle").click(
+    function() {
+      runScriptV2();
+    });
+
+  // 2.0.0 Script
+  function runScriptV2() {
+    // Add version, excluded names, and emptyAdded
+    chrome.storage.local.set({
+      "version": 2,
+      "manualCoupleList": $("#manualCoupleList").val(),
+    });
+    checkEmptyAdded();
+
+    // Execute content script
+    chrome.scripting.executeScript(
+      {
+        target: { tabId },
+        files: ["content-script.js"],
+      },
+      () => {
+        // Get the stored values
+        chrome.storage.local.get(
+          [
+            "memberCount",
+            "manCoupleCount",
+            "soloRandom",
+            "allRandom",
+          ],
+          function (data) {
+            // Add counts
+            if($('#counts')) {
+              $("#counts").remove();
+            }
+            $('#input-container2').before('<div class="input-container" id="counts"></div>');
+            $('#counts').append("<b> 총 멤버 수: " + data.memberCount + "<br> 제외된 멤버 수: " + data.manCoupleCount + "</b>");
+
+            $("#V2output1").val(data.soloRandom);
+            $("#V2output2").val(data.allRandom);
+          }
+        );
+      })
+
+  };
 });
